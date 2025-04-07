fe14e1ab43c9f9dd4fcfcf8f77778127e22a12a2
Anastasia Konushok <nastjenka.kon@gmail.com>
2025-04-07 09:49:41 +0300

    feat(app): add routing and HomePage setup

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export const HomePage = () => (
+    <Box p={6}>
+        <Text fontSize='3xl' fontWeight='bold'>
+            Приятного аппетита!
+        </Text>
+    </Box>
+);
